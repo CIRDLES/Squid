9908d635c6f2459c10d18a6a64f910c57eab78b7
Jim Bowring <bowring@gmail.com>
2018-01-01 08:11:45 -0500

    updates copyright notices to just name year of origin

--- squidCore/src/main/java/org/cirdles/squid/core/PrawnFileHandler.java
+++ squidCore/src/main/java/org/cirdles/squid/core/PrawnFileHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 CIRDLES
+ * Copyright 2016 James F. Bowring and CIRDLES.org.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
