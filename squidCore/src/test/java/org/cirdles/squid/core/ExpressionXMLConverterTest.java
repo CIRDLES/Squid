ac7258e104c1917fb0a154fd946798822ab7ded4
ryanbarrett1515 <barrettra@g.cofc.edu>
2018-03-21 13:53:50 -0400

    xmlconvertertest has been verified to work and updated etc

--- squidCore/src/test/java/org/cirdles/squid/core/ExpressionXMLConverterTest.java
+++ squidCore/src/test/java/org/cirdles/squid/core/ExpressionXMLConverterTest.java
@@ -7,12 +7,15 @@ package org.cirdles.squid.core;
 
 import com.thoughtworks.xstream.XStream;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.List;
+import org.cirdles.squid.shrimp.SquidSpeciesModel;
 import org.cirdles.squid.tasks.expressions.Expression;
 import org.cirdles.squid.tasks.expressions.customExpressions.CustomExpression_LnUO_U;
+import org.cirdles.squid.tasks.expressions.expressionTrees.ExpressionTree;
+import org.cirdles.squid.tasks.expressions.expressionTrees.ExpressionTreeInterface;
+import org.cirdles.squid.tasks.expressions.functions.Function;
+import org.cirdles.squid.tasks.expressions.isotopes.ShrimpSpeciesNode;
+import org.cirdles.squid.tasks.expressions.operations.Operation;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.input.SAXBuilder;
@@ -31,17 +34,31 @@ public class ExpressionXMLConverterTest {
         try {
             XStream xstream = new XStream();
             Expression initialExpression = new Expression(new CustomExpression_LnUO_U(), CustomExpression_LnUO_U.excelExpressionString, true);
-            initialExpression.customizeXstream(xstream);
-            File initialFile = new File("src/test/resources/org/cirdles/squid/XMLSerializationTest/InitialCreation.XML");
-            FileOutputStream fosInitial = new FileOutputStream(initialFile);
-            PrintWriter initialWriter = new PrintWriter(fosInitial);
-            initialWriter.print(xstream.toXML(initialExpression));
 
-            Expression convertedExpression = (Expression) xstream.fromXML(initialFile);
-            File convertedFile = new File("src/test/resources/org/cirdles/squid/XMLSerializationTest/ConvertedCreation.XML");
-            FileOutputStream fosConverted = new FileOutputStream(convertedFile);
-            PrintWriter convertedWriter = new PrintWriter(fosConverted);
-            convertedWriter.print(xstream.toXML(convertedExpression));
+            SquidSpeciesModel sm1 = new SquidSpeciesModel(0, "254", "254", "Uranium", false, "");
+            SquidSpeciesModel sm2 = new SquidSpeciesModel(0, "238", "238", "Uranium", false, "");
+
+            ExpressionTreeInterface sn1 = ShrimpSpeciesNode.buildShrimpSpeciesNode(sm1, "getPkInterpScanArray");
+            ExpressionTreeInterface sn2 = ShrimpSpeciesNode.buildShrimpSpeciesNode(sm2, "getPkInterpScanArray");
+
+            ExpressionTreeInterface rm = new ExpressionTree("254/238", sn1, sn2, Operation.divide());
+
+            ExpressionTree expTree = (ExpressionTree) initialExpression.getExpressionTree();
+
+            expTree.getRatiosOfInterest().clear();
+            expTree.getRatiosOfInterest().add("254/238");
+            expTree.setOperation(Function.ln());
+            expTree.getChildrenET().clear();
+            expTree.addChild(0, rm);
+
+            initialExpression.customizeXstream(xstream);
+            File initialFile = new File("InitialCreation.XML");
+
+            initialExpression.serializeXMLObject(initialFile.getAbsolutePath());
+
+            Expression convertedExpression = (Expression) (new Expression()).readXMLObject(initialFile.getAbsolutePath(), false);
+            File convertedFile = new File("ConvertedCreation.XML");
+            convertedExpression.serializeXMLObject(convertedFile.getAbsolutePath());
 
             SAXBuilder builder = new SAXBuilder();
             Document initialDocument = (Document) builder.build(initialFile);
@@ -49,51 +66,34 @@ public class ExpressionXMLConverterTest {
             Element initialRootElement = initialDocument.getRootElement();
             Element convertedRootElement = convertedDocument.getRootElement();
 
-            List<Element> initialElements = getAllLeafNodes(initialRootElement);
-            List<Element> convertedElements = getAllLeafNodes(convertedRootElement);
-
-            assertTrue(compareAll(initialElements, convertedElements));
+            assertTrue(compareElements(initialRootElement, convertedRootElement));
         } catch (Exception e) {
             e.printStackTrace();
             fail("Exception thrown: " + e.getMessage());
         }
     }
 
-    private List<Element> getAllLeafNodes(Element passedElement) {
-        List<Element> returnElements = new ArrayList<>();
-        List<Element> elementParents = new ArrayList<>();
-        elementParents.add(passedElement);
-
-        while (!elementParents.isEmpty()) {
-            Element currentElement = elementParents.remove(0);
-            List<Element> currentElements = currentElement.getChildren();
-            if (currentElements.isEmpty()) {
-                returnElements.add(currentElement);
-            } else {
-                elementParents.addAll(currentElements.subList(0, currentElements.size()));
-            }
-        }
-
-        return returnElements;
-    }
-
-    private boolean compareAll(List<Element> firstList, List<Element> secondList) {
+    private boolean compareElements(Element firstElement, Element secondElement) {
         boolean returnValue = true;
+        List<Element> firstList = firstElement.getChildren();
+        List<Element> secondList = secondElement.getChildren();
 
-        int i = 0;
-        while (returnValue && i < firstList.size()) {
-            boolean foundEqualNode = false;
-            int j = 0;
-            while (!foundEqualNode && j < secondList.size()) {
-                if (firstList.get(i).equals(secondList.get(j))) {
-                    foundEqualNode = true;
+        if (firstList.size() == secondList.size()) {
+            if (!firstList.isEmpty()) {
+                while (!firstList.isEmpty()) {
+                    Element firstChild = firstList.remove(0);
+                    Element secondChild = secondList.remove(0);
+                    if (compareElements(firstChild, secondChild) == false) {
+                        returnValue = false;
+                    }
                 }
-                j++;
+            } else {
+                if (!(firstElement.getText().equals(secondElement.getText()))) {
+                    returnValue = false;
+                }
             }
-            if (!foundEqualNode) {
-                returnValue = false;
-            }
-            i++;
+        } else {
+            returnValue = false;
         }
 
         return returnValue;
