cbaf5cfc70837398076b8174cfcf8f27111ef4c3
ryanbarrett1515 <barrettra@g.cofc.edu>
2018-05-02 16:55:20 -0400

    test updated to allow deletion of custom expressions

--- squidCore/src/test/java/org/cirdles/squid/core/ExpressionXMLConverterTest.java
+++ squidCore/src/test/java/org/cirdles/squid/core/ExpressionXMLConverterTest.java
@@ -7,6 +7,7 @@ package org.cirdles.squid.core;
 
 import com.thoughtworks.xstream.XStream;
 import java.io.File;
+import java.util.LinkedList;
 import java.util.List;
 import org.cirdles.squid.shrimp.SquidSpeciesModel;
 import org.cirdles.squid.tasks.expressions.Expression;
@@ -33,7 +34,18 @@ public class ExpressionXMLConverterTest {
     public void ExpressionXMLConverterTest() {
         try {
             XStream xstream = new XStream();
-            Expression initialExpression = new Expression(new CustomExpression_LnUO_U(), CustomExpression_LnUO_U.excelExpressionString, true);
+            ExpressionTree LnUOU = new ExpressionTree("LnUO/U");
+            List<String> ratiosOfInterest = new LinkedList<>();
+            ratiosOfInterest.add("254/238");
+            LnUOU.setRatiosOfInterest(ratiosOfInterest);
+            LnUOU.setOperation(Function.ln());
+            LnUOU.setRootExpressionTree(true);
+            LnUOU.setSquidSwitchSCSummaryCalculation(false);
+            LnUOU.setSquidSwitchSTReferenceMaterialCalculation(true);
+            LnUOU.setSquidSwitchSAUnknownCalculation(true);
+            
+            Expression initialExpression = new Expression(LnUOU, "ln([\"254/238\"])", true);
+            
 
             SquidSpeciesModel sm1 = new SquidSpeciesModel(0, "254", "254", "Uranium", false, "");
             SquidSpeciesModel sm2 = new SquidSpeciesModel(0, "238", "238", "Uranium", false, "");
