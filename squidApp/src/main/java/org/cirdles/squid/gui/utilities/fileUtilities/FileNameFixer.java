b4f9cf34b52d10a637a18cf802c07370fd52a197
ryanbarrett1515 <barrettra@g.cofc.edu>
2018-05-30 13:41:20 -0400

    updates

--- squidApp/src/main/java/org/cirdles/squid/gui/utilities/fileUtilities/FileNameFixer.java
+++ squidApp/src/main/java/org/cirdles/squid/gui/utilities/fileUtilities/FileNameFixer.java
@@ -1,7 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Copyright 2018 James F. Bowring and CIRDLES.org.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.cirdles.squid.gui.utilities.fileUtilities;
 
@@ -12,7 +22,7 @@ import java.io.File;
  * @author ryanb
  */
 public class FileNameFixer {
-    
+
     public static String fixFileName(String name) {
         name = name.replaceAll(" ", "_");
         name = name.replaceAll("\\^", "");
@@ -43,10 +53,10 @@ public class FileNameFixer {
         while (name.contains("__")) {
             name = name.replaceAll("__", "_");
         }
-        
+
         return name;
     }
-    
+
     public static String fixFileName(File file) {
         return fixFileName(file.toString());
     }
