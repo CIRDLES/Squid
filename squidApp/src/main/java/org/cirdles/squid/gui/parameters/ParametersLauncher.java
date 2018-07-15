e2acf25a89bebff1573b5c0e06d9cd5ffcd0e397
ryanbarrett1515 <barrettra@g.cofc.edu>
2018-07-15 00:18:15 -0400

    current (not perfect) parameters manager integrated

--- squidApp/src/main/java/org/cirdles/squid/gui/parameters/ParametersLauncher.java
+++ squidApp/src/main/java/org/cirdles/squid/gui/parameters/ParametersLauncher.java
@@ -7,13 +7,9 @@ package org.cirdles.squid.gui.parameters;
 
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 import javafx.stage.WindowEvent;
-import org.cirdles.squid.gui.SquidUI;
 
 /**
  *
@@ -22,34 +18,31 @@ import org.cirdles.squid.gui.SquidUI;
 public class ParametersLauncher {
 
     public ParametersLauncher() {
-        
-    };
+
+    }
+
+    ;
 
     public void launchParametersManager() {
-        try {
-            Stage stage = SquidUI.parametersStage;
+        try{
+        Stage stage = new Stage();;
+        
+        stage.setMinHeight(700);
+        stage.setMinWidth(800);
+        
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("SquidParametersManagerGUI.fxml")));
+        
+        stage.setScene(scene);
+        stage.setTitle("Squid Parameters Manager");
 
-            stage.setMinHeight(700);
-            stage.setMinWidth(800);
-
-            Parent root = new AnchorPane();
-            Scene scene = new Scene(FXMLLoader.load(getClass().getResource("SquidParametersManagerGUI.fxml")));
-
-            stage.setScene(scene);
-            stage.setTitle("Squid Parameters Manager");
-
-            SquidUI.parametersWindow = stage.getScene().getWindow();
-
-            stage.setOnCloseRequest((WindowEvent e) -> {
-                Platform.exit();
-                System.exit(0);
-            });
-
-            stage.show();
-            
-            SquidUI.parametersWindow.requestFocus();
-        } catch (Exception e) {
-
+        stage.setOnCloseRequest((WindowEvent e) -> {
+            Platform.exit();
+            System.exit(0);
+        });
+        
+        stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
         }
     }
 }
