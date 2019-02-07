30fc5644f17d4717ce98f0385bb4526f9f1cbe3d
ryanbarrett1515 <barrettra@g.cofc.edu>
2019-02-07 18:31:35 -0500

    html file building progress (html file created and some not so pretty information displayed, image not showing correctly in html file)

--- squidCore/src/main/java/org/cirdles/squid/tasks/expressions/ExpressionPublisher.java
+++ squidCore/src/main/java/org/cirdles/squid/tasks/expressions/ExpressionPublisher.java
@@ -1,34 +1,29 @@
 package org.cirdles.squid.tasks.expressions;
 
-import java.awt.Color;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-
-import com.google.common.io.CharStreams;
 import org.cirdles.commons.util.ResourceExtractor;
 import org.cirdles.squid.projects.SquidProject;
 import org.cirdles.squid.utilities.stateUtilities.SquidSerializer;
+import org.scilab.forge.jlatexmath.TeXFormula;
 
+import javax.imageio.ImageIO;
+import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
-import javax.imageio.ImageIO;
-import javax.xml.transform.Source;
-import org.scilab.forge.jlatexmath.TeXFormula;
+import java.nio.Buffer;
 
 public class ExpressionPublisher {
 
     private static final TransformerFactory transformerFactory = TransformerFactory.newInstance();
 
     public static String removeMathMLStyling(String input) {
-       // System.out.println("Input:\n" + input);
         String output = input.replaceAll("(<.*(mstyle|mspace).*>|&nbsp;)", "");
-        //output = output.replaceAll("&nbsp;", "&#160;");
         output = output.replaceAll("&times;", "*");
-       // System.out.println("Output:\n" + output);
         return output;
     }
 
@@ -37,7 +32,7 @@ public class ExpressionPublisher {
         if (hasStyling) {
             input = removeMathMLStyling(input);
         }
-        try { 
+        try {
             input = "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" display=\"block\">" + input + "</math>";
             StringWriter resultWriter = new StringWriter();
             StreamResult result = new StreamResult(resultWriter);
@@ -45,7 +40,7 @@ public class ExpressionPublisher {
             Source inputSource = new StreamSource(inputReader);
 
             Transformer transformer = transformerFactory.newTransformer(new StreamSource("xsltml_2.0/mmltex.xsl"));
-            transformer.transform(inputSource, result);            
+            transformer.transform(inputSource, result);
 
             retVal = resultWriter.toString().replaceAll("\\%", "\\\\%");
         } catch (TransformerException e) {
@@ -62,38 +57,55 @@ public class ExpressionPublisher {
         return formula.createBufferedImage(1, 50, Color.BLUE, Color.LIGHT_GRAY);
     }
 
-    public static void main(String[] argv) {
-
-        /*String xmlExpressionFileName = "Hf1sabserr.xml";
-        String xsltExpressionTransformer = "mmltex.xsl";
-        // Create a transform factory instance.
-        TransformerFactory tfactory = TransformerFactory.newInstance();
+    public static boolean createHTMLDocumentFromExpression(File file, Expression exp) {
+        BufferedImage image = (BufferedImage) createImageFromMathML(exp.getExpressionTree().toStringMathML(), true);
+        File imageFile = new File(exp.getName()+ ".jpeg");
+        boolean imageCreated;
         try {
-            // Create a transformer for the stylesheet.
-            Transformer transformer
-                    = tfactory.newTransformer(new StreamSource(xsltExpressionTransformer));
-            // Transform the source XML to System.out.
-            transformer.transform(new StreamSource(xmlExpressionFileName),
-                    new StreamResult(System.out));
-            transformer.transform(new StreamSource(xmlExpressionFileName),
-                    new StreamResult("LATEX.txt"));
-        } catch (TransformerException transformerException) {
+            ImageIO.write(image, "jpeg", imageFile);
+            imageCreated = true;
+        } catch(IOException e) {
+            e.printStackTrace();
+            imageCreated = false;
         }
-        String converted = "$\\sqrt{{\\left(\\frac{\\text{Hfsens1sigabs[0]}}{\\text{Hfsens[0]}}\\right)}^{2}+{\\left(\\frac{{\\left(\\frac{{\\text{195.9}}_{\\text{HfO}}^{\\text{}}}{{\\text{195.8}}_{\\text{Zr2O}}^{\\text{}}}\\right)}^{\\text{\\%}}}{100}\\right)}^{2}}*\\text{Hfppm[0]}$";
-        TeXFormula formula = new TeXFormula(converted);
-        formula.createJPEG(1, 50, "LATEX.jpeg", Color.white, Color.black);*/
+        StringBuilder string = new StringBuilder();
+        string.append("<HTML>");
+        string.append("<h1>" + exp.getName() + "</h1>");
+        if(imageCreated) {
+            string.append("<img src=" + imageFile.getAbsolutePath() + "/>");
+        }
+        string.append("<body>");
+        string.append("<p>");
+        string.append("Expression Tree Audit: " + exp.produceExpressionTreeAudit() + "</p>");
+        string.append("<p>Excel Expression String: " + exp.getExcelExpressionString() + "</p>");
+        string.append("<p>Notes:\n" + (exp.getNotes().trim().isEmpty()? "No notes" : exp.getNotes()) + "</p>");
+        string.append("</body>");
+        string.append("</HTML>");
+
+        boolean worked;
+        try {
+            PrintWriter writer = new PrintWriter(new FileOutputStream(file));
+            writer.write(string.toString());
+            writer.flush();
+            worked = true;
+        } catch(IOException e) {
+            worked = false;
+        }
+        return worked;
+    }
+
+    public static void main(String[] argv) {
         ResourceExtractor squidProjectExtractor = new ResourceExtractor(SquidProject.class);
         File squidFile = squidProjectExtractor.extractResourceAsFile("Z626611PKPERM1.squid");
         SquidProject project = (SquidProject) SquidSerializer.getSerializedObjectFromFile(squidFile.getAbsolutePath(), false);
         Expression exp = project.getTask().getExpressionByName("Hf1sabserr");
-        //Expression exp = (Expression) (new Expression()).readXMLObject("Hf1sabserr.xml", false);
-
-        BufferedImage image = (BufferedImage) createImageFromMathML(exp.getExpressionTree().toStringMathML(), true);
+        createHTMLDocumentFromExpression(new File("test.html"), exp);
+        /*BufferedImage image = (BufferedImage) createImageFromMathML(exp.getExpressionTree().toStringMathML(), true);
         try {
-        ImageIO.write(image, "jpeg", new File("test.jpeg"));
+            ImageIO.write(image, "jpeg", new File("test.jpeg"));
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
 }
