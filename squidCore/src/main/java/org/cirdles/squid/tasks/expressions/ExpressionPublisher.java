ca213f3be895135d486e01cee2f9a56872c42ae2
ryanbarrett1515 <barrettra@g.cofc.edu>
2019-02-19 13:20:55 -0500

    didn't find any ways of making equation span multiple lines (that work with the mathml - latex converter) but can make pdf wider, need method of finding out how much wider to make it (pdf renderer uses inches as unit)

--- squidCore/src/main/java/org/cirdles/squid/tasks/expressions/ExpressionPublisher.java
+++ squidCore/src/main/java/org/cirdles/squid/tasks/expressions/ExpressionPublisher.java
@@ -17,11 +17,15 @@ import javax.xml.transform.stream.StreamSource;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+import org.antlr.v4.misc.MutableInt;
 import org.cirdles.squid.utilities.fileUtilities.FileNameFixer;
 
 public class ExpressionPublisher {
 
     private static final TransformerFactory transformerFactory = TransformerFactory.newInstance();
+    private static final double SIZE_MULTIPLIER = .4;
 
     public static String removeMathMLStyling(String input) {
         String output = input.replaceAll("(<.*(mstyle|mspace).*>|&nbsp;)", "");
@@ -60,29 +64,52 @@ public class ExpressionPublisher {
         return formula.createBufferedImage(1, 60, Color.BLUE, Color.WHITE);
     }
 
-    public static boolean createHTMLDocumentFromExpressions(File file, List<Expression> expressions) {
-        StringBuilder string = new StringBuilder();
-        string.append("<!DOCTYPE html>\n");
-        string.append("<HTML lang=\"en\">\n<head>\n<meta charset=\"utf-8\"/>\n");
-        string.append("<title>Squid Expressions</title>\n");
-        string.append("<style>\n"
+    private static String getStartHTML() {
+        return "<!DOCTYPE html>\n"
+                + "<HTML lang=\"en\">\n<head>\n<meta charset=\"utf-8\"/>\n"
+                + "<title>Squid Expressions</title>\n"
+                + "<style>\n"
                 + "body {background-color: white;}\n"
                 + "h1   {color: blue;}\n"
                 + "h2   {color: blue}\n"
                 + "p    {color: blue;}\n"
-                + "</style>\n");
-        string.append("</head>\n<body>\n");
-        string.append("<h1>Expressions</h1>\n");
+                + "</style>\n"
+                + "</head>\n<body>\n"
+                + "<h1>Expressions</h1>\n";
+    }
+
+    private static String getEndHTML() {
+        return "</body>\n</HTML>";
+    }
+
+    private static String getExpressionInfoHTML(Expression exp) {
+        return "<p>" + exp.getName() + " is " + (exp.isReferenceMaterialValue() ? "" : "not ") + "a reference material value\n"
+                + "<br/>" + exp.getName() + " is " + (exp.isParameterValue() ? "" : "not ") + "a parameter value\n"
+                + "<br/>" + exp.getName() + " is " + (exp.amHealthy() ? "" : "not ") + "healthy\n"
+                + "<br/>" + exp.getName() + " is " + (exp.isSquidSwitchNU() ? "" : "not ") + "Squid Switch NU</p>\n"
+                + "<p>Excel Expression String: " + exp.getExcelExpressionString() + "</p>\n"
+                + "<p>Notes:<br/>" + (exp.getNotes().trim().isEmpty() ? "No notes" : exp.getNotes()).replaceAll("\n", "<br/>") + "</p>\n";
+    }
+
+    public static boolean createHTMLDocumentFromExpressions(File file, List<Expression> expressions, Integer[] widths) {
+        boolean enterWidths = widths != null;
+
+        StringBuilder string = new StringBuilder();
+        string.append(getStartHTML());
 
         File imageFolder = new File("expressionsImages");
         imageFolder.mkdir();
 
-        for (Expression exp : expressions) {
+        for (int i = 0; i < expressions.size(); i++) {
+            Expression exp = expressions.get(i);
             BufferedImage image = (BufferedImage) createImageFromMathML(exp.getExpressionTree().toStringMathML(), true);
             File imageFile = new File(imageFolder.getPath() + File.separator + FileNameFixer.fixFileName(exp.getName()) + ".png");
             boolean imageCreated;
             try {
                 ImageIO.write(image, "png", imageFile);
+                if (enterWidths) {
+                    widths[i] = image.getWidth();
+                }
                 imageCreated = true;
             } catch (IOException e) {
                 e.printStackTrace();
@@ -90,19 +117,13 @@ public class ExpressionPublisher {
             }
             string.append("<h2>" + exp.getName() + "</h2>\n");
             if (imageCreated) {
-                string.append("<img src=\"" + imageFile.getPath().replaceAll("\\\\", "/") + "\" alt=\"Image not available\" width=\"" + ((int) (image.getWidth() / 2.5 + .5)) + "\" "
-                        + "height=\"" + ((int) (image.getHeight() / 2.5 + .5)) + "\"" + "/>\n");
+                string.append("<img src=\"" + imageFile.getPath().replaceAll("\\\\", "/") + "\" alt=\"Image not available\" width=\""
+                        + ((int) (image.getWidth() * SIZE_MULTIPLIER + .5)) + "\" "
+                        + "height=\"" + ((int) (image.getHeight() * SIZE_MULTIPLIER + .5)) + "\"" + "/>\n");
             }
-            string.append("<p>" + exp.getName() + " is " + (exp.isReferenceMaterialValue() ? "" : "not ") + "a reference material value\n");
-            string.append("<br/>" + exp.getName() + " is " + (exp.isParameterValue() ? "" : "not ") + "a parameter value\n");
-            string.append("<br/>" + exp.getName() + " is " + (exp.amHealthy() ? "" : "not ") + "healthy\n");
-            string.append("<br/>" + exp.getName() + " is " + (exp.isSquidSwitchNU() ? "" : "not ") + "Squid Switch NU</p>\n");
-            string.append("<p>Excel Expression String: " + exp.getExcelExpressionString() + "</p>\n");
-            string.append("<p>Notes:<br/>" + (exp.getNotes().trim().isEmpty() ? "No notes" : exp.getNotes()).replaceAll("\n", "<br/>") + "</p>\n");
-            //string.append("<p>Expression Tree Audit:<br/>" + exp.produceExpressionTreeAudit().replaceAll("\n", "<br/>") + "</p>\n");
+            string.append(getExpressionInfoHTML(exp));
         }
-        string.append("</body>\n");
-        string.append("</HTML>");
+        string.append(getEndHTML());
 
         boolean worked;
         try {
@@ -116,42 +137,31 @@ public class ExpressionPublisher {
         return worked;
     }
 
-    public static boolean createHTMLDocumentFromExpression(File file, Expression exp) {
+    public static boolean createHTMLDocumentFromExpression(File file, Expression exp, MutableInt width) {
         BufferedImage image = (BufferedImage) createImageFromMathML(exp.getExpressionTree().toStringMathML(), true);
         File imageFile = new File(FileNameFixer.fixFileName(exp.getName()) + ".png");
         boolean imageCreated;
         try {
             ImageIO.write(image, "png", imageFile);
+            if (width != null) {
+                width.v = image.getWidth();
+            }
             imageCreated = true;
         } catch (IOException e) {
             e.printStackTrace();
             imageCreated = false;
         }
         StringBuilder string = new StringBuilder();
-        string.append("<!DOCTYPE html>\n");
-        string.append("<HTML lang=\"en\">\n<head>\n<meta charset=\"utf-8\"/>\n");
-        string.append("<title>Squid Expressions</title>\n");
-        string.append("<style>\n"
-                + "body {background-color: white;}\n"
-                + "h1   {color: blue;}\n"
-                + "h2   {color: blue}\n"
-                + "p    {color: blue;}\n"
-                + "</style>\n");
-        string.append("</head>\n<body>\n");
+        string.append(getStartHTML());
+
         string.append("<h1>" + exp.getName() + "</h1>\n");
         if (imageCreated) {
-            string.append("<img src=\"" + imageFile.getPath() + "\" alt=\"Image not available\" width=\"" + ((int) (image.getWidth() / 2.5 + .5)) + "\" "
-                    + "height=\"" + ((int) (image.getHeight() / 2.5 + .5)) + "\"" + "/>\n");
+            string.append("<img src=\"" + imageFile.getPath() + "\" alt=\"Image not available\" width=\"" + ((int) (image.getWidth() * SIZE_MULTIPLIER + .5)) + "\" "
+                    + "height=\"" + ((int) (image.getHeight() * SIZE_MULTIPLIER + .5)) + "\"" + "/>\n");
         }
-        string.append("<p>" + exp.getName() + " is " + (exp.isReferenceMaterialValue() ? "" : "not ") + "a reference material value\n");
-        string.append("<br/>" + exp.getName() + " is " + (exp.isParameterValue() ? "" : "not ") + "a parameter value\n");
-        string.append("<br/>" + exp.getName() + " is " + (exp.amHealthy() ? "" : "not ") + "healthy\n");
-        string.append("<br/>" + exp.getName() + " is " + (exp.isSquidSwitchNU() ? "" : "not ") + "Squid Switch NU</p>\n");
-        string.append("<p>Excel Expression String: " + exp.getExcelExpressionString() + "</p>\n");
-        string.append("<p>Notes:<br/>" + (exp.getNotes().trim().isEmpty() ? "No notes" : exp.getNotes()) + "</p>\n");
-        //string.append("<p>Expression Tree Audit:<br/>" + exp.produceExpressionTreeAudit().replaceAll("\n", "<br/>") + "</p>\n");
-        string.append("</body>\n");
-        string.append("</HTML>\n");
+        string.append(getExpressionInfoHTML(exp));
+
+        string.append(getEndHTML());
 
         boolean worked;
         try {
@@ -167,42 +177,53 @@ public class ExpressionPublisher {
 
     public static boolean createPDFFromExpressions(File file, List<Expression> expressions) {
         boolean retVal;
+        Integer[] widths = new Integer[expressions.size()];
 
         File htmlFile = new File("ExpressionsHTMLToPDFConversionFile.html");
-        retVal = createHTMLDocumentFromExpressions(htmlFile, expressions);
+        retVal = createHTMLDocumentFromExpressions(htmlFile, expressions, widths);
 
         if (retVal) {
             try {
                 OutputStream os = new FileOutputStream(file);
                 PdfRendererBuilder builder = new PdfRendererBuilder();
+                double max = Collections.max(Arrays.asList(widths)) * SIZE_MULTIPLIER;
+                double width = (max > PdfRendererBuilder.PAGE_SIZE_LETTER_WIDTH) ? max : PdfRendererBuilder.PAGE_SIZE_LETTER_WIDTH;
+               // builder.useDefaultPageSize((float) width, PdfRendererBuilder.PAGE_SIZE_LETTER_HEIGHT, PdfRendererBuilder.PageSizeUnits.INCHES);
                 builder.withFile(htmlFile);
                 builder.toStream(os);
                 builder.run();
                 retVal = true;
             } catch (Exception e) {
                 e.printStackTrace();
+                retVal = false;
             }
         }
         htmlFile.delete();
+
         return retVal;
     }
 
     public static boolean createPDFFromExpression(File file, Expression exp) {
         boolean retVal;
+        MutableInt width = new MutableInt(0);
 
         File htmlFile = new File("ExpressionHTMLToPDFConversionFile.html");
-        retVal = createHTMLDocumentFromExpression(htmlFile, exp);
+        retVal = createHTMLDocumentFromExpression(htmlFile, exp, width);
 
         if (retVal) {
             try {
                 OutputStream os = new FileOutputStream(file);
                 PdfRendererBuilder builder = new PdfRendererBuilder();
+                builder.useDefaultPageSize((width.v > PdfRendererBuilder.PAGE_SIZE_LETTER_WIDTH)
+                        ? width.v : PdfRendererBuilder.PAGE_SIZE_LETTER_WIDTH,
+                        PdfRendererBuilder.PAGE_SIZE_LETTER_HEIGHT, PdfRendererBuilder.PageSizeUnits.INCHES);
                 builder.withFile(htmlFile);
                 builder.toStream(os);
                 builder.run();
                 retVal = true;
             } catch (Exception e) {
                 e.printStackTrace();
+                retVal = false;
             }
         }
         htmlFile.delete();
@@ -214,8 +235,8 @@ public class ExpressionPublisher {
         File squidFile = squidProjectExtractor.extractResourceAsFile("Z626611PKPERM1.squid");
         SquidProject project = (SquidProject) SquidSerializer.getSerializedObjectFromFile(squidFile.getAbsolutePath(), false);
         Expression exp = project.getTask().getExpressionByName("Hf1sabserr");
-        createHTMLDocumentFromExpression(new File("test.html"), exp);
-        createHTMLDocumentFromExpressions(new File("testgroup.html"), project.getTask().getTaskExpressionsOrdered());
+        createHTMLDocumentFromExpression(new File("test.html"), exp, null);
+        createHTMLDocumentFromExpressions(new File("testgroup.html"), project.getTask().getTaskExpressionsOrdered(), null);
         createPDFFromExpression(new File("test.pdf"), exp);
         createPDFFromExpressions(new File("testgroup.pdf"), project.getTask().getTaskExpressionsOrdered());
     }
