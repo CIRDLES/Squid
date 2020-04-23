0a6de959cc47bc07fb6929c13059a2d91bd47f98
ryanbarrett1515 <barrettra@g.cofc.edu>
2020-04-23 15:38:57 -0400

    removes delimitters and parameters section in task designer, adds set defaults button and implements all functionality in project manager

--- squidApp/src/main/java/org/cirdles/squid/gui/ProjectManagerController.java
+++ squidApp/src/main/java/org/cirdles/squid/gui/ProjectManagerController.java
@@ -27,7 +27,9 @@ import javafx.util.StringConverter;
 import org.cirdles.squid.constants.Squid3Constants;
 import org.cirdles.squid.parameters.parameterModels.ParametersModel;
 import org.cirdles.squid.tasks.TaskInterface;
+import org.cirdles.squid.tasks.taskDesign.TaskDesign;
 import org.cirdles.squid.utilities.squidPrefixTree.SquidPrefixTree;
+import org.cirdles.squid.utilities.stateUtilities.SquidPersistentState;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -41,10 +43,11 @@ import static org.cirdles.squid.utilities.conversionUtilities.RoundingUtilities.
  * FXML Controller class
  *
  * @author James F. Bowring
- * @see <a href="https://courses.bekwam.net/public_tutorials/bkcourse_filterlistapp.html" target="_blank">Bekwam.net</a>
+ * @see
+ * <a href="https://courses.bekwam.net/public_tutorials/bkcourse_filterlistapp.html" target="_blank">Bekwam.net</a>
  */
 public class ProjectManagerController implements Initializable {
-
+    
     @FXML
     private TextField orignalPrawnFileName;
     @FXML
@@ -103,10 +106,11 @@ public class ProjectManagerController implements Initializable {
     private Spinner<Double> assignedExternalErrUSpinner;
     @FXML
     private Spinner<Double> assignedExternalErrThSpinner;
-
-
+    @FXML
+    private Button parametersSetDefaultsButton;
+    
     private TaskInterface task;
-
+    private TaskDesign taskDesign;
 
     /**
      * Initializes the controller class.
@@ -117,6 +121,7 @@ public class ProjectManagerController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         task = squidProject.getTask();
+        taskDesign = SquidUIController.squidPersistentState.getTaskDesign();
         if (task != null) {
             roundingSquid3.setSelected(task.isRoundingForSquid3());
             setUpParametersModelsComboBoxes();
@@ -131,26 +136,26 @@ public class ProjectManagerController implements Initializable {
                 spotAverageRatioCalcRadioButton.setSelected(true);
             }
         }
-
+        
         orignalPrawnFileName.setEditable(false);
-
+        
         setupListeners();
 
         // detect if project opened from menu by deserialization
         if (squidProject.prawnFileExists()) {
             setUpPrawnFile();
         }
-
+        
         titleLabel.setStyle(STYLE_MANAGER_TITLE);
     }
-
+    
     private void setUpParametersModelsComboBoxes() {
 
         // PhysicalConstantsModels
         physConstModelComboBox.setConverter(new ParameterModelStringConverter());
         physConstModelComboBox.setItems(FXCollections.observableArrayList(squidLabData.getPhysicalConstantsModels()));
         physConstModelComboBox.getSelectionModel().select(task.getPhysicalConstantsModel());
-
+        
         physConstModelComboBox.valueProperty()
                 .addListener((ObservableValue<? extends ParametersModel> observable, ParametersModel oldValue, ParametersModel newValue) -> {
                     task.setPhysicalConstantsModel(newValue);
@@ -162,7 +167,7 @@ public class ProjectManagerController implements Initializable {
         commonPbModelComboBox.setConverter(new ParameterModelStringConverter());
         commonPbModelComboBox.setItems(FXCollections.observableArrayList(squidLabData.getCommonPbModels()));
         commonPbModelComboBox.getSelectionModel().select(task.getCommonPbModel());
-
+        
         commonPbModelComboBox.valueProperty()
                 .addListener((ObservableValue<? extends ParametersModel> observable, ParametersModel oldValue, ParametersModel newValue) -> {
                     task.setCommonPbModel(newValue);
@@ -170,21 +175,20 @@ public class ProjectManagerController implements Initializable {
                     task.setupSquidSessionSpecsAndReduceAndReport(false);
                 });
     }
-
-
+    
     static class ParameterModelStringConverter extends StringConverter<ParametersModel> {
-
+        
         @Override
         public String toString(ParametersModel model) {
             return model.getModelNameWithVersion();
         }
-
+        
         @Override
         public ParametersModel fromString(String string) {
             return null;
         }
     }
-
+    
     private void setupListeners() {
         projectNameText.textProperty().addListener(new ChangeListener<String>() {
             @Override
@@ -192,23 +196,23 @@ public class ProjectManagerController implements Initializable {
                 squidProject.setProjectName(newValue);
             }
         });
-
+        
         analystNameText.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
                 squidProject.setAnalystName(newValue);
             }
         });
-
+        
         projectNotesText.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
                 squidProject.setProjectNotes(newValue);
             }
         });
-
+        
         pb208RadioButton.setVisible(!task.isDirectAltPD() && !task.getParentNuclide().contains("232"));
-
+        
         switch (task.getSelectedIndexIsotope()) {
             case PB_204:
                 pb204RadioButton.setSelected(true);
@@ -220,107 +224,107 @@ public class ProjectManagerController implements Initializable {
                 pb208RadioButton.setSelected(true);
                 break;
         }
-
+        
         autoExcludeSpotsCheckBox.setSelected(task.isSquidAllowsAutoExclusionOfSpots());
-
+        
         SpinnerValueFactory<Double> valueFactoryU
                 = new SpinnerValueFactory.DoubleSpinnerValueFactory(0.50, 1.00, task.getExtPErrU(), 0.05);
         assignedExternalErrUSpinner.setValueFactory(valueFactoryU);
         assignedExternalErrUSpinner.valueProperty().addListener((ObservableValue<? extends Double> observable,
-                                                                 Double oldValue, Double newValue) -> {
+                Double oldValue, Double newValue) -> {
             task.setExtPErrU(newValue);
         });
-
+        
         SpinnerValueFactory<Double> valueFactoryTh
                 = new SpinnerValueFactory.DoubleSpinnerValueFactory(0.50, 1.00, task.getExtPErrTh(), 0.05);
         assignedExternalErrThSpinner.setValueFactory(valueFactoryTh);
         assignedExternalErrThSpinner.valueProperty().addListener((ObservableValue<? extends Double> observable, //
-                                                                  Double oldValue, Double newValue) -> {
+                Double oldValue, Double newValue) -> {
             task.setExtPErrTh(newValue);
         });
     }
-
+    
     private void setUpPrawnFile() {
         // first prepare the session
         squidProject.preProcessPrawnSession();
-
+        
         projectNameText.setText(squidProject.getProjectName());
         analystNameText.setText(squidProject.getAnalystName());
         projectNotesText.setText(squidProject.getProjectNotes());
-
+        
         orignalPrawnFileName.setText(squidProject.getPrawnSourceFilePath());
-
+        
         squidFileNameText.setText(SquidUIController.projectFileName);
-
+        
         softwareVersionLabel.setText(
                 "Version: "
-                        + squidProject.getPrawnFileShrimpSoftwareVersionName());
-
+                + squidProject.getPrawnFileShrimpSoftwareVersionName());
+        
         loginCommentLabel.setText(
                 "Login Comment: "
-                        + squidProject.getPrawnFileLoginComment());
-
+                + squidProject.getPrawnFileLoginComment());
+        
         extractSummaryStatsFromPrawnFile();
     }
-
+    
     private void extractSummaryStatsFromPrawnFile() {
-
+        
         SquidPrefixTree spotPrefixTree = squidProject.generatePrefixTreeFromSpotNames();
-
+        
         String summaryStatsString = spotPrefixTree.buildSummaryDataString();
 
         // format into rows for summary tab
         summaryStatsLabel.setText("Summary:\n\t" + summaryStatsString.replaceAll(";", "\n\t"));
-
+        
         totalAnalysisTimeLabel.setText("Total time in hours = " + (int) squidProject.getSessionDurationHours());
     }
-
+    
     @FXML
     private void yesSBMRadioButtonAction(ActionEvent event) {
         task.setUseSBM(true);
         task.setChanged(true);
         task.setupSquidSessionSpecsAndReduceAndReport(true);
     }
-
+    
     @FXML
     private void noSBMRadioButtonActions(ActionEvent event) {
         task.setUseSBM(false);
         task.setChanged(true);
         task.setupSquidSessionSpecsAndReduceAndReport(true);
     }
-
+    
     @FXML
     private void linearRegressionRatioCalcRadioButtonAction(ActionEvent event) {
         task.setUserLinFits(true);
         task.setChanged(true);
         task.setupSquidSessionSpecsAndReduceAndReport(true);
     }
-
+    
     @FXML
     private void spotAverageRatioCalcRadioButtonAction(ActionEvent event) {
         task.setUserLinFits(false);
         task.setChanged(true);
         task.setupSquidSessionSpecsAndReduceAndReport(true);
     }
-
+    
     @FXML
     private void pb204RadioButtonAction(ActionEvent event) {
         task.setSelectedIndexIsotope(Squid3Constants.IndexIsoptopesEnum.PB_204);
         task.setChanged(true);
     }
-
+    
     @FXML
     private void pb207RadioButtonAction(ActionEvent event) {
         task.setSelectedIndexIsotope(Squid3Constants.IndexIsoptopesEnum.PB_207);
         task.setChanged(true);
     }
-
+    
     @FXML
     private void pb208RadioButtonAction(ActionEvent event) {
         task.setSelectedIndexIsotope(Squid3Constants.IndexIsoptopesEnum.PB_208);
         task.setChanged(true);
     }
-
+    
     @FXML
     private void roundingSquid25Action(ActionEvent event) {
         useSigFig15 = false;
@@ -328,7 +332,7 @@ public class ProjectManagerController implements Initializable {
         task.setChanged(true);
         task.setupSquidSessionSpecsAndReduceAndReport(true);
     }
-
+    
     @FXML
     private void roundingSquid3Action(ActionEvent event) {
         useSigFig15 = true;
@@ -336,16 +340,41 @@ public class ProjectManagerController implements Initializable {
         task.setChanged(true);
         task.setupSquidSessionSpecsAndReduceAndReport(true);
     }
-
+    
     @FXML
     private void autoExcludeSpotsCheckBoxAction(ActionEvent event) {
         // this will cause weighted mean expressions to be changed with boolean flag
         task.updateRefMatCalibConstWMeanExpressions(autoExcludeSpotsCheckBox.isSelected());
     }
+    
+    @FXML
+    private void parametersSetDefaultsOnAction(ActionEvent actionEvent) {
+        taskDesign.setUseSBM(yesSBMRadioButton.isSelected());
+        
+        taskDesign.setUserLinFits(linearRegressionRatioCalcRadioButton.isSelected());
 
+        if (pb204RadioButton.isSelected()) {
+            taskDesign.setSelectedIndexIsotope(Squid3Constants.IndexIsoptopesEnum.PB_204);
+        } else if (pb207RadioButton.isSelected()) {
+            taskDesign.setSelectedIndexIsotope(Squid3Constants.IndexIsoptopesEnum.PB_207);
+        } else {
+            taskDesign.setSelectedIndexIsotope(Squid3Constants.IndexIsoptopesEnum.PB_208);
+        }
+        
+       taskDesign.setSquidAllowsAutoExclusionOfSpots(autoExcludeSpotsCheckBox.isSelected());
+       
+       taskDesign.setExtPErrTh(assignedExternalErrThSpinner.getValue());
+       taskDesign.setExtPErrU(assignedExternalErrUSpinner.getValue());
+       
+       taskDesign.setPhysicalConstantsModel(physConstModelComboBox.getValue());
+       taskDesign.setCommonPbModel(commonPbModelComboBox.getValue());
+       
+       SquidUIController.squidPersistentState.updateSquidPersistentState();
+    }
+    
     @FXML
     private void refreshModelsAction(ActionEvent event) {
         task.refreshParametersFromModels(true, true, false);
     }
-
+    
 }
