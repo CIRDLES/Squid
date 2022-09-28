7dbac60026d06606f286c15fcb0a3e30125efc67
Jim Bowring <bowring@gmail.com>
2022-09-28 16:41:10 -0400

    Fixed #734

--- squidApp/src/main/java/org/cirdles/squid/gui/TaskEditorController.java
+++ squidApp/src/main/java/org/cirdles/squid/gui/TaskEditorController.java
@@ -90,76 +90,19 @@ public class TaskEditorController implements Initializable {
     private static final ToggleGroup denTG = new ToggleGroup();
     public static TaskInterface existingTaskToEdit = null;
     public static Squid3Constants.TaskEditTypeEnum editType = Squid3Constants.TaskEditTypeEnum.EDIT_CURRENT;
-    private static TaskDesign taskEditor;
+    private  TaskDesign taskEditor;
     @FXML
-    private static TextField uncorrConstPbUExpressionText;
+    private  TextField uncorrConstPbUExpressionText;
     @FXML
-    private static TextField uncorrConstPbThExpressionText;
+    private  TextField uncorrConstPbThExpressionText;
     @FXML
-    private static TextField parentConcExpressionText;
+    private  TextField parentConcExpressionText;
     @FXML
-    private static TextField pb208Th232ExpressionText;
+    private  TextField pb208Th232ExpressionText;
     @FXML
-    private static TextFlow defaultRatiosListTextFlow;
+    private TextFlow defaultRatiosListTextFlow;
     private static Map<String, ExpressionTreeInterface> namedExpressionsMap = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
     private static boolean amGeochronMode;
-
-    static {
-        addBtnHBox.setAlignment(Pos.CENTER);
-        addBtn.setStyle(
-                "-fx-padding: 5 22 5 22;\n"
-                        + "    -fx-border-color: #e2e2e2;"
-                        + "    -fx-border-width: 2;"
-                        + "    -fx-background-radius: 0;"
-                        + "    -fx-background-color: #FB6D42;"
-                        + "    -fx-font-family: SansSerif;"
-                        + "    -fx-font-size: 11pt;"
-                        + "    -fx-text-fill: whitesmoke;"
-                        + "    -fx-background-insets: 0 0 0 0, 0, 1, 2;");
-
-        addBtn.setOnMouseClicked((event) -> {
-            taskEditor.addRatioName(numLabel.getText() + "/" + denLabel.getText());
-            namedExpressionsMap = taskEditor.buildNamedExpressionsMap();
-            populateRatios();
-            refreshExpressionAudits();
-            updateAddButton();
-        });
-
-        updateAddButton();
-
-        numLabel.setStyle("-fx-font-family: SansSerif bold;-fx-font-size: 18");
-        divLabel.setStyle("-fx-font-family: SansSerif bold;-fx-font-size: 18");
-        denLabel.setStyle("-fx-font-family: SansSerif bold;-fx-font-size: 18");
-
-        infoLabelHBox.setAlignment(Pos.CENTER);
-        AnchorPane pane = new AnchorPane(menuVBox);
-        AnchorPane.setBottomAnchor(menuVBox, 0.0);
-        AnchorPane.setTopAnchor(menuVBox, 0.0);
-        AnchorPane.setRightAnchor(menuVBox, 0.0);
-        AnchorPane.setLeftAnchor(menuVBox, 0.0);
-
-        numTG.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
-            public void changed(ObservableValue<? extends Toggle> ob, Toggle o, Toggle n) {
-                RadioButton rb = (RadioButton) numTG.getSelectedToggle();
-                if (rb != null) {
-                    numLabel.setText(rb.getText());
-                    updateAddButton();
-                }
-            }
-        });
-        denTG.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
-            public void changed(ObservableValue<? extends Toggle> ob, Toggle o, Toggle n) {
-                RadioButton rb = (RadioButton) denTG.getSelectedToggle();
-                if (rb != null) {
-                    denLabel.setText(rb.getText());
-                    updateAddButton();
-                }
-            }
-        });
-        ADD_RATIOS_STAGE.setScene(new Scene(pane, 250, 500));
-        ADD_RATIOS_STAGE.setAlwaysOnTop(true);
-
-    }
 
     private final List<StackPane> undoMassesList = new ArrayList<>();
     EventHandler<MouseEvent> mouseEnteredExpressionEventHandler = new EventHandler<MouseEvent>() {
@@ -262,7 +205,7 @@ public class TaskEditorController implements Initializable {
         return ratio;
     }
 
-    private static void updateAddButton() {
+    private  void updateAddButton() {
         String num = numLabel.getText();
         String den = denLabel.getText();
         boolean valid = (num.compareTo(den) != 0)
@@ -272,7 +215,7 @@ public class TaskEditorController implements Initializable {
         addBtn.setDisable(!valid);
     }
 
-    private static void populateRatios() {
+    private  void populateRatios() {
         defaultRatiosListTextFlow.getChildren().clear();
         defaultRatiosListTextFlow.setMaxHeight(30);
         int count = 1;
@@ -305,7 +248,7 @@ public class TaskEditorController implements Initializable {
         }
     }
 
-    private static void refreshExpressionAudits() {
+    private  void refreshExpressionAudits() {
         Expression exp = makeExpression(UNCOR206PB238U_CALIB_CONST, uncorrConstPbUExpressionText.getText());
         tooltipMapPut(exp, UNCOR206PB238U_CALIB_CONST);
         updateExpressionHealthFlag(uncorrConstPbUExpressionText, exp.amHealthy());
@@ -355,6 +298,61 @@ public class TaskEditorController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        addBtnHBox.setAlignment(Pos.CENTER);
+        addBtn.setStyle(
+                "-fx-padding: 5 22 5 22;\n"
+                        + "    -fx-border-color: #e2e2e2;"
+                        + "    -fx-border-width: 2;"
+                        + "    -fx-background-radius: 0;"
+                        + "    -fx-background-color: #FB6D42;"
+                        + "    -fx-font-family: SansSerif;"
+                        + "    -fx-font-size: 11pt;"
+                        + "    -fx-text-fill: whitesmoke;"
+                        + "    -fx-background-insets: 0 0 0 0, 0, 1, 2;");
+
+        addBtn.setOnMouseClicked((event) -> {
+            taskEditor.addRatioName(numLabel.getText() + "/" + denLabel.getText());
+            namedExpressionsMap = taskEditor.buildNamedExpressionsMap();
+            populateRatios();
+            refreshExpressionAudits();
+            updateAddButton();
+        });
+
+        updateAddButton();
+
+        numLabel.setStyle("-fx-font-family: SansSerif bold;-fx-font-size: 18");
+        divLabel.setStyle("-fx-font-family: SansSerif bold;-fx-font-size: 18");
+        denLabel.setStyle("-fx-font-family: SansSerif bold;-fx-font-size: 18");
+
+        infoLabelHBox.setAlignment(Pos.CENTER);
+        AnchorPane pane = new AnchorPane(menuVBox);
+        AnchorPane.setBottomAnchor(menuVBox, 0.0);
+        AnchorPane.setTopAnchor(menuVBox, 0.0);
+        AnchorPane.setRightAnchor(menuVBox, 0.0);
+        AnchorPane.setLeftAnchor(menuVBox, 0.0);
+
+        numTG.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            public void changed(ObservableValue<? extends Toggle> ob, Toggle o, Toggle n) {
+                RadioButton rb = (RadioButton) numTG.getSelectedToggle();
+                if (rb != null) {
+                    numLabel.setText(rb.getText());
+                    updateAddButton();
+                }
+            }
+        });
+        denTG.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            public void changed(ObservableValue<? extends Toggle> ob, Toggle o, Toggle n) {
+                RadioButton rb = (RadioButton) denTG.getSelectedToggle();
+                if (rb != null) {
+                    denLabel.setText(rb.getText());
+                    updateAddButton();
+                }
+            }
+        });
+
+        ADD_RATIOS_STAGE.setScene(new Scene(pane, 250, 500));
+        ADD_RATIOS_STAGE.setAlwaysOnTop(true);
+
         titleLabel.setStyle(STYLE_MANAGER_TITLE);
         try {
             taskEditor = SquidPersistentState.getExistingPersistentState().getTaskDesign();
